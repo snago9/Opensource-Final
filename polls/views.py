a32fc39cdc82f8d73fa6a031a393f7b042dcc2aa
snago9 <snago9@naver.com>
2022-06-21 22:13:02 +0900

    Third Commit

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -10,20 +11,23 @@ def index(request):
    # context = {'latest_question_list': latest_question_list}
    # return render(request, 'polls/index.html', context)
 
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    #context = {'latest_question_list': latest_question_list}
+    #return HttpResponse(template.render(context, request))
 
-def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+   # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+   # output = ','.join([q.question_text for q in latest_question_list])
+   # return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    #return HttpResponse("You're looking at question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of questions %s"
